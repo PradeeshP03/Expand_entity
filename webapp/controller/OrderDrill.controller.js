e0778f0d5ff5d21e1108ed04232f7c949a530019
pradeesh p <pradeesh4187@gmail.com>
2025-07-29 12:07:31 +0000

    update

--- webapp/controller/OrderDrill.controller.js
+++ webapp/controller/OrderDrill.controller.js
@@ -5,6 +5,22 @@ sap.ui.define([
  
     return BaseController.extend("demo.ladera.expandentityui5.controller.OrderDrill", {
         onInit() {
+            sap.ui.core.UIComponent.getRouterFor(this).getRoute("OrderDrill").attachPatternMatched(this._objPatternMatched,this);
+          },
+          _objPatternMatched : function(evt){
+            var orderId = evt.getParameter("arguments").OrderId;
+            this.getOwnerComponent().getModel().read("/Orders",{
+              urlParameters : {
+                "$expand" : "Customer,Order_Details"
+              },
+              filters:[new sap.ui.model.Filter("OrderID","EQ",orderId)],
+              success:function(oData,results){
+                console.log(oData);
+              },
+              error:function(oError){
+                MessageBox.error(JSON.parse(oError.responseText).error.message.value)
+              }     
+            })
           },
         onNavigationBack:function(){
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
